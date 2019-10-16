dc9cbaad43a6680c46726a74f7890f65bf4212b5
Anwesh43 <anweshthecool0@gmail.com>
2019-10-16 09:47:14 +0530

    adding constants and libraries

--- bimidlineview/src/main/java/com/anwesh/uiprojects/bimidlineview/BiMidLineView.kt
+++ bimidlineview/src/main/java/com/anwesh/uiprojects/bimidlineview/BiMidLineView.kt
@@ -4,3 +4,18 @@ package com.anwesh.uiprojects.bimidlineview
  * Created by anweshmishra on 16/10/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
