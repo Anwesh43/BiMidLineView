763a9e27a74f45a413298e27b623b610b1527c99
Anwesh43 <anweshthecool0@gmail.com>
2019-10-16 10:19:10 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbimidlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbimidlineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedbimidlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.bimidlineview.BiMidLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BiMidLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
