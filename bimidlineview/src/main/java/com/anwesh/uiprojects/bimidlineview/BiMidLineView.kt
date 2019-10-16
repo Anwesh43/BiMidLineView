2529241b315e3166f7ee8db8c00b240c904b0f7e
Anwesh43 <anweshthecool0@gmail.com>
2019-10-16 10:00:10 +0530

    adding BiMidLineView class

--- bimidlineview/src/main/java/com/anwesh/uiprojects/bimidlineview/BiMidLineView.kt
+++ bimidlineview/src/main/java/com/anwesh/uiprojects/bimidlineview/BiMidLineView.kt
@@ -51,3 +51,21 @@ fun Canvas.drawBMLNode(i : Int, scale : Float, paint : Paint) {
     drawBiMidLine(scale, size, w, paint)
     restore()
 }
+
+class BiMidLineView(ctx : Context) : View(ctx) {
+
+    private val  paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
