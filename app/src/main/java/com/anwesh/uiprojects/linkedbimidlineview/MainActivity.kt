01ec79da0cee1f9431d202774f3534fa9c889e7f
Anwesh43 <anweshthecool0@gmail.com>
2019-10-16 10:17:07 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbimidlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbimidlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbimidlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bimidlineview.BiMidLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiMidLineView.create(this)
     }
 }
