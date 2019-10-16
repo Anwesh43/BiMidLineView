bbe33881cce61d2730aa765349ca295ab05129f8
Anwesh43 <anweshthecool0@gmail.com>
2019-10-16 09:58:36 +0530

    adding Canvas extension method

--- bimidlineview/src/main/java/com/anwesh/uiprojects/bimidlineview/BiMidLineView.kt
+++ bimidlineview/src/main/java/com/anwesh/uiprojects/bimidlineview/BiMidLineView.kt
@@ -19,8 +19,35 @@ val sizeFactor : Float = 2.9f
 val delay : Long = 30
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 3
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawBiMidLine(scale : Float, size : Float, w : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 3)
+    val sc2 : Float = scale.divideScale(1, 3)
+    val sc3 : Float = scale.divideScale(2, 3)
+    val xMid : Float = w / 2
+    drawLine(0f, (size), xMid * sc1, size, paint)
+    save()
+    translate(xMid, 0f)
+    drawLine(0f, size, 0f, size - 2 * size * sc2, paint)
+    drawLine(xMid, -size, xMid + xMid * sc3, -size, paint)
+    restore()
+}
+
+fun Canvas.drawBMLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(0f, gap * (i + 1))
+    drawBiMidLine(scale, size, w, paint)
+    restore()
+}
